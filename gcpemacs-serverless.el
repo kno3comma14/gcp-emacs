993c7a77ef3fc1d3c00f68e33c80a409014d9fa8
Enyert <enyert.watson@gmail.com>
2020-01-26 19:44:45 -0500

    Adding gitignore file and gcloud functions list command support

---
+++ gcpemacs-serverless.el
@@ -0,0 +1,28 @@
+; NAME STATUS TRIGGER REGION
+
+(setq gcloud-functions-list-command "gcloud functions list")
+(setq gcloud-functions-list-command-keys '("NAME" "STATUS" "TRIGGER" "REGION"))
+
+(defun call-command-without-headers (x)
+  (butlast (cdr (split-string (shell-command-to-string x) "\n")) 1))
+
+(defun create-single-functions-list-data (x)
+  (let ((values (split-string x))
+	(function_map (make-hash-table :test 'equal)))    
+    (puthash "NAME" (nth 0 values) function_map)
+    (puthash "STATUS" (nth 1 values) function_map)
+    (puthash "TRIGGER" (nth 2 values) function_map)
+    (puthash "REGION" (nth 3 values) function_map)
+    function_map))
+
+(defun create-functions-list-data ()
+  (let ((rows (call-command-without-headers gcloud-functions-list-command)))
+    (mapcar #'create-single-functions-list-data rows)))
+
+
+
+
+
+
+
+
